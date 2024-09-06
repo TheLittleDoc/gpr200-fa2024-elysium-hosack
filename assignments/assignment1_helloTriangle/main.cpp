df22f233e90671032b8eb4b0307bc66debb15867
Elysium Hosack <elysium.hosack@mymail.champlain.edu>
2024-09-06 12:46:31 -0400

    Created *green triangle following the LearnOpenGL.com tutorials

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -1,3 +1,15 @@
+/******************************************************************************
+* File name: main.cpp       File author(s): Professor Winebrenner(?)          *
+*                                           Mod. by Elysium Hosack            *
+* File description:                                                           *
+* Using Eric's framework from assignment 1. Goal is to render a classic Hello *
+* Triangle window, plus our own little twist.                                 *
+*                                                                             *
+* Cloned: 9/6/2024          Edited last: 9/6/2024                             *
+*                                                                             *
+******************************************************************************/
+
+#include <iostream>
 #include <stdio.h>
 #include <math.h>
 
@@ -6,7 +18,7 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
-const int SCREEN_WIDTH = 1080;
+const int SCREEN_WIDTH = 960;
 const int SCREEN_HEIGHT = 720;
 
 int main() {
@@ -15,7 +27,7 @@ int main() {
 		printf("GLFW failed to init!");
 		return 1;
 	}
-	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Serinity - Hello Triangle", NULL, NULL);
 	if (window == NULL) {
 		printf("GLFW failed to create window");
 		return 1;
@@ -26,14 +38,110 @@ int main() {
 		return 1;
 	}
 	//Initialization goes here!
-	
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f,
+	};
+
+	// Vertex Shader
+	// from learnopengl.com
+
+	const char *vertexShaderSource =	"#version 330 core										\n"
+										"layout (location = 0) in vec3 aPos;					\n"
+										"void main()											\n"
+										"{														\n"
+										"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);	\n"
+										"}														\0";
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Fragment Shader
+	// from learnopengl.com
+
+	const char *fragmentShaderSource =	"#version 330 core									\n"
+										"out vec4 FragColor;								\n"
+										"void main()										\n"
+										"{													\n"
+										"   FragColor = vec4(0.4f, 0.8f, 0.08f, 1.0f);		\n"
+										"}													\0";
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Shader program
+	// also from learnopengl.com
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	glUseProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
 	//Render loop
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		//Clear framebuffer
-		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
+		glClearColor(0.3f, 0.4f, 0.7f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		//Drawing happens here!
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
